44c9e9b228e688278808fa9098a82f769dc393d7
AbroShahzeb <khizr77@gmail.com>
2025-01-12 22:01:23 +0500

    changes

--- src/pages/home/components/WhyUsSection.jsx
+++ src/pages/home/components/WhyUsSection.jsx
@@ -29,8 +29,8 @@ const whyUsCards = [
 
 export const WhyUsSection = () => {
   return (
-    <section className="px-6 pb-[120px] md:pb-[144px] lg:pb-[200px] md:px-10 lg:px-20 ">
-      <div className="bg-[#2C343E] md:bg-bg-steps px-6 pt-16 pb-[64px] md:px-[74px] md:pt-[56px] md:pb-[70px]  rounded-t-[10px] lg:pt-[100px] lg:pb-[86px]">
+    <section className="px-6 pb-[120px] md:pb-[144px] lg:pb-[200px] md:px-10 lg:px-20">
+      <div className="bg-[#2C343E] md:bg-bg-steps bg-cover px-6 pt-16 pb-[64px] md:px-[74px] md:pt-[56px] md:pb-[70px]  rounded-t-[10px] lg:pt-[100px] lg:pb-[86px]">
         <div className="flex flex-col items-center text-center gap-6 lg:gap-8 text-surface md:max-w-[495px] lg:max-w-[540px] md:mx-auto">
           <h2 className="text-[28px] leading-[28px] md:text-[32px] md:leading-[48px] lg:text-[40px]  font-fraunces font-black">
             Why choose us?
@@ -45,7 +45,7 @@ export const WhyUsSection = () => {
       </div>
 
       <div className="relative">
-        <div className="absolute top-0 left-0 right-0 h-1/2 xl:h-[calc(50%+36px)] w-full rounded-b-[10px] z-10 bg-bg-steps bg-contain"></div>
+        <div className="absolute top-0 left-0 right-0 h-1/2 xl:h-[calc(50%+36px)] w-full rounded-b-[10px] z-10 bg-bg-steps bg-cover"></div>
         <div className=" flex flex-col items-center gap-8 relative  z-20 px-6 md:px-0 xl:flex-row xl:px-[85px] xl:gap-[30px] xl:items-stretch xl:h-[388px]">
           {whyUsCards.map((card) => (
             <WhyUsCard card={card} />
