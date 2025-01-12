17b1311699b8a51b072fbbf08e41cf7563be8bb2
AbroShahzeb <khizr77@gmail.com>
2025-01-12 21:06:02 +0500

    changes

--- src/pages/home/components/CollectionItem.jsx
+++ src/pages/home/components/CollectionItem.jsx
@@ -7,9 +7,17 @@ export const CollectionItem = ({ item }) => {
   const imageVariants = {
     hidden: { opacity: 0, y: 0, rotate: 0 }, // Start off-screen and slightly down
     shown: {
-      opacity: 1,
+      opacity: [0, 0.5, 1],
       y: [50, -20, 0], // Keyframes for jump effect
       rotate: [0, 10, 0], // Keyframes for tilt
+    },
+  };
+
+  const contentVariants = {
+    hidden: { opacity: 0, x: 0 },
+    shown: {
+      opacity: [0, 0.5, 1],
+      x: [70, -10, 0],
     },
   };
 
@@ -19,7 +27,7 @@ export const CollectionItem = ({ item }) => {
       className="flex flex-col items-center text-center text-dark-grey-blue md:flex-row md:justify-center md:gap-[36px] xl:gap-0  md:text-left xl:flex-col lg:text-center w-full"
     >
       <motion.div
-        className="flex items-center justify-center"
+        className="flex items-center justify-center relative z-50"
         ref={ref} // Attach observer to track visibility
         initial="hidden" // Starting animation state
         animate={inView ? "shown" : "hidden"} // Trigger animation based on visibility
@@ -27,6 +35,7 @@ export const CollectionItem = ({ item }) => {
         transition={{
           duration: 1.2, // Total animation time
           ease: "easeInOut", // Smooth transitions
+          times: [0, 0.3, 1],
         }}
       >
         <img
@@ -35,14 +44,26 @@ export const CollectionItem = ({ item }) => {
           alt={item.title}
         />
       </motion.div>
-      <div className="mt-6 md:mt-0 lg:mt-0">
+      <motion.div
+        ref={ref}
+        variants={contentVariants}
+        initial="hidden" // Starting animation state
+        animate={inView ? "shown" : "hidden"} // Trigger animation based on visibility
+        transition={{
+          duration: 1.2, // Total animation time
+          ease: "easeInOut", // Smooth transitions
+          times: [0, 0.3, 1],
+          delay: 0.5,
+        }}
+        className="mt-6 md:mt-0 lg:mt-0"
+      >
         <h3 className="text-heading-4 font-fraunces font-black ">
           {item.title}
         </h3>
         <p className="mt-4 md:mt-6 text-body max-w-[282px]">
           {item.description}
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
