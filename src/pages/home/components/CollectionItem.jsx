dc6eed681297d796802029c0f2869b225b373799
AbroShahzeb <khizr77@gmail.com>
2025-01-12 21:52:16 +0500

    changes

--- src/pages/home/components/CollectionItem.jsx
+++ src/pages/home/components/CollectionItem.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 export const CollectionItem = ({ item }) => {
-  const { ref, inView } = useInView({ threshold: 0.4, triggerOnce: true });
+  const { ref, inView } = useInView({ threshold: 0.4 });
 
   const imageVariants = {
     hidden: { opacity: 0, y: 0, rotate: 0 }, // Start off-screen and slightly down
@@ -26,14 +26,14 @@ export const CollectionItem = ({ item }) => {
       key={item.id}
       className="flex flex-col items-center text-center text-dark-grey-blue md:flex-row md:justify-center md:gap-[36px] xl:gap-0  md:text-left xl:flex-col lg:text-center w-full"
     >
-      <div
+      <motion.div
         className="flex items-center justify-center relative z-50"
         ref={ref} // Attach observer to track visibility
-        initial="shown" // Starting animation state
+        initial="hidden" // Starting animation state
         animate={inView ? "shown" : "hidden"} // Trigger animation based on visibility
         variants={imageVariants} // Define animation keyframes
         transition={{
-          duration: 0.1, // Total animation time
+          duration: 1.2, // Total animation time
           ease: "easeInOut", // Smooth transitions
           times: [0, 0.3, 1],
         }}
@@ -43,19 +43,19 @@ export const CollectionItem = ({ item }) => {
           className="w-[200px] h-[151px] md:w-[255px] md:h-[193px] xl:h-[183px] xl:w-auto xl:ml-2"
           alt={item.title}
         />
-      </div>
-      <div
+      </motion.div>
+      <motion.div
         ref={ref}
         variants={contentVariants}
-        initial="shown" // Starting animation state
+        initial="hidden" // Starting animation state
         animate={inView ? "shown" : "hidden"} // Trigger animation based on visibility
         transition={{
-          duration: 0.1, // Total animation time
+          duration: 1.2, // Total animation time
           ease: "easeInOut", // Smooth transitions
           times: [0, 0.3, 1],
           delay: 0.5,
         }}
-        className="mt-6 md:mt-0 lg:mt-[72px]"
+        className="mt-6 md:mt-0 xl:mt-[72px]"
       >
         <h3 className="text-heading-4 font-fraunces font-black ">
           {item.title}
@@ -63,7 +63,7 @@ export const CollectionItem = ({ item }) => {
         <p className="mt-4 md:mt-6 text-body max-w-[282px]">
           {item.description}
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
